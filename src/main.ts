d4819a1e3a14f69821c4ec6cf11aa0fbfeca5107
m-zs <mzasada93@gmail.com>
2021-09-16 21:22:41 +0200

    chore: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
+
+import { swaggerConfig } from './swagger.config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT || 3000);
 }
+
 bootstrap();
