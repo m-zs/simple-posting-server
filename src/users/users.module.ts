a1adf9cad8412edf62f5f2591292df5e6d3c7564
m-zs <mzasada93@gmail.com>
2021-09-17 15:34:12 +0200

    feat: add user component structure

---
+++ src/users/users.module.ts
@@ -0,0 +1,9 @@
+import { Logger, Module } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+
+@Module({
+  controllers: [UsersController],
+  providers: [UsersService, Logger],
+})
+export class UsersModule {}
