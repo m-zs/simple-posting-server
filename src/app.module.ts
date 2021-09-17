45236ee0d9858f3da1a930b99bbee43f89b04b08
m-zs <mzasada93@gmail.com>
2021-09-17 12:32:16 +0200

    feat: add typeorm cfg

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { configService } from './config';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(configService.getTypeOrmConfig())],
   controllers: [],
   providers: [],
 })
