cdd5475d684289ca61de3ef35432c6144964e012
m-zs <mzasada93@gmail.com>
2021-09-17 19:13:16 +0200

    chore: add create-user dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,26 @@
-export class CreateUserDto {}
+import {
+  IsEmail,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+const STRONG_PASSWORD_REGEX =
+  /(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/;
+
+export class CreateUserDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  username!: string;
+
+  @IsEmail()
+  email!: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(25)
+  @Matches(STRONG_PASSWORD_REGEX)
+  password!: string;
+}
