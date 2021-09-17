a9b9fadada0bb38b23f2c1e48ce022ffab3ea175
m-zs <mzasada93@gmail.com>
2021-09-17 15:55:44 +0200

    chore: add user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,19 @@
-export class User {}
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryColumn('uuid')
+  id!: string;
+
+  @Column({ unique: true })
+  username!: string;
+
+  @Column({ unique: true })
+  email!: string;
+
+  @Column()
+  password!: string;
+
+  @Column()
+  sessionVersion!: string;
+}
