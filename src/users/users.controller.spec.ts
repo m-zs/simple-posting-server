d2e2b555f18302e48bd489f7573d0e1803233ddb
m-zs <mzasada93@gmail.com>
2021-09-18 12:40:48 +0200

    test: add controller createUser unit tests

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,20 +1,46 @@
+import { Logger } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { UsersController } from './users.controller';
+import { UsersRepository } from './users.repository';
 import { UsersService } from './users.service';
 
 describe('UsersController', () => {
-  let controller: UsersController;
+  let usersController: UsersController;
+  let usersService: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        {
+          provide: UsersRepository,
+          useClass: jest.fn(),
+        },
+        { provide: Logger, useClass: jest.fn() },
+        UsersService,
+      ],
     }).compile();
 
-    controller = module.get<UsersController>(UsersController);
+    usersController = module.get<UsersController>(UsersController);
+    usersService = module.get<UsersService>(UsersService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('createUser', () => {
+    it('should return void', async () => {
+      const expectedResult = undefined;
+      const createSpy = jest
+        .spyOn(usersService, 'createUser')
+        .mockImplementation(() => Promise.resolve(expectedResult));
+
+      const result = await usersController.createUser({
+        username: '',
+        email: '',
+        password: '',
+      });
+
+      expect(createSpy).toHaveBeenCalledTimes(1);
+      expect(result).toBe(expectedResult);
+    });
   });
 });
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,31 @@
+import { Logger } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+
+import { UsersRepository } from './users.repository';
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
-  let service: UsersService;
+  let usersService: UsersService;
+  let usersRepository: UsersRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        {
+          provide: UsersRepository,
+          useClass: jest.fn(() => ({})),
+        },
+        { provide: Logger, useClass: jest.fn() },
+        UsersService,
+      ],
     }).compile();
 
-    service = module.get<UsersService>(UsersService);
+    usersService = module.get<UsersService>(UsersService);
+    usersRepository = module.get<UsersRepository>(UsersRepository);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(usersService).toBeDefined();
+    expect(usersRepository).toBeDefined();
   });
 });
