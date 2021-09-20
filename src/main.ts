fd169719ac696ff76773eba0fdab5d6050f83c8b
m-zs <mzasada93@gmail.com>
2021-09-20 12:21:08 +0200

    feat: add all exceptions filter, adjust folder structure

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { WinstonModule } from 'nest-winston';
 import { SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
@@ -6,7 +6,8 @@ import { ValidationPipe } from '@nestjs/common';
 import { configService } from './config';
 import { swaggerConfig } from './swagger.config';
 import { DEV_LOGGER_CFG, PROD_LOGGER_CFG } from './logger';
-import { TransformInterceptor } from './transform-interceptor';
+import { TransformInterceptor } from './shared/interceptors/transform-interceptor';
+import { AllExceptionsFilter } from './shared/filters/all-exceptions.filter';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -16,9 +17,11 @@ async function bootstrap() {
   );
   const app = await NestFactory.create(AppModule, { logger });
   const document = SwaggerModule.createDocument(app, swaggerConfig);
+  const { httpAdapter } = app.get(HttpAdapterHost);
 
   SwaggerModule.setup('api', app, document);
   app.useGlobalInterceptors(new TransformInterceptor());
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(port);
---
+++ src/shared/filters/all-exceptions.filter.ts
@@ -0,0 +1,11 @@
+import { Catch, ArgumentsHost, Logger } from '@nestjs/common';
+import { BaseExceptionFilter } from '@nestjs/core';
+
+@Catch()
+export class AllExceptionsFilter extends BaseExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    Logger.error(exception);
+
+    super.catch(exception, host);
+  }
+}
