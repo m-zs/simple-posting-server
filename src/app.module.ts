c3f6f8ace37096293b846556baa6c730012a99a0
m-zs <mzasada93@gmail.com>
2021-09-20 21:33:12 +0200

    feat: add auth base

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { configService } from './config';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
     UsersModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
