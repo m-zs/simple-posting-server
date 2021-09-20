284fb3f4430adc58cbddf9928334e9b7f7b12c55
m-zs <mzasada93@gmail.com>
2021-09-20 16:17:38 +0200

    refactor: remove winston logging for now

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,17 @@
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
-import { WinstonModule } from 'nest-winston';
 import { SwaggerModule } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 import { configService } from './config';
 import { swaggerConfig } from './swagger.config';
-import { DEV_LOGGER_CFG, PROD_LOGGER_CFG } from './logger';
 import { TransformInterceptor } from './shared/interceptors/transform-interceptor';
 import { AllExceptionsFilter } from './shared/filters/all-exceptions.filter';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const port = configService.getPort();
-  const logger = WinstonModule.createLogger(
-    configService.isProduction() ? PROD_LOGGER_CFG : DEV_LOGGER_CFG,
-  );
-  const app = await NestFactory.create(AppModule, { logger });
+
+  const app = await NestFactory.create(AppModule);
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   const { httpAdapter } = app.get(HttpAdapterHost);
 
@@ -25,7 +21,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(port);
-  logger.log(`Listening at port: ${port}`);
+  Logger.log(`Listening at port: ${port}`);
 }
 
 bootstrap();
--- src/shared/filters/all-exceptions.filter.ts
+++ src/shared/filters/all-exceptions.filter.ts
@@ -1,19 +1,10 @@
-import {
-  Catch,
-  ArgumentsHost,
-  Logger,
-  InternalServerErrorException,
-} from '@nestjs/common';
+import { Catch, ArgumentsHost } from '@nestjs/common';
 import { BaseExceptionFilter } from '@nestjs/core';
 
 @Catch()
 export class AllExceptionsFilter extends BaseExceptionFilter {
   catch(exception: unknown, host: ArgumentsHost) {
-    const { url, method } = host.getArgByIndex(0);
-
-    if (exception instanceof InternalServerErrorException) {
-      Logger.error(`Error occured on: [${method}]: ${url}`, exception);
-    }
+    // handle log storage here
 
     super.catch(exception, host);
   }
