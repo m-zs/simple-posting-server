449e3741e52a3eafef7724a216e1740f8ff54fe6
m-zs <mzasada93@gmail.com>
2021-09-23 10:11:39 +0200

    test: add unit test for signIn auth service

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,104 @@
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { AuthService } from './auth.service';
+import { UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Test, TestingModule } from '@nestjs/testing';
+import * as bcrypt from 'bcrypt';
+
+import { MockType } from 'src/types';
+import { UsersRepository } from 'src/users/users.repository';
+import { AuthService } from './auth.service';
+
+jest.mock('bcrypt');
 
 describe('AuthService', () => {
-  // let service: AuthService;
+  let authService: AuthService;
+  let usersRepository: MockType<UsersRepository>;
+  let jwtService: MockType<JwtService>;
 
-  // beforeEach(async () => {
-  //   const module: TestingModule = await Test.createTestingModule({
-  //     providers: [AuthService],
-  //   }).compile();
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UsersRepository,
+          useFactory: jest.fn(() => ({
+            findUserByUsername: jest.fn(),
+            updateSession: jest.fn(),
+          })),
+        },
+        {
+          provide: JwtService,
+          useFactory: jest.fn(() => ({
+            sign: jest.fn(),
+          })),
+        },
+      ],
+    }).compile();
 
-  //   service = module.get<AuthService>(AuthService);
-  // });
+    authService = module.get<AuthService>(AuthService);
+    usersRepository = module.get(UsersRepository);
+    jwtService = module.get(JwtService);
+  });
 
-  it('should be defined', () => {
-    expect(true).toBe(true);
+  describe('signIn', () => {
+    it('should return expected value and update session', async () => {
+      const user = {
+        id: '',
+        username: '',
+        password: '',
+        userSession: '',
+      };
+      const token = 'token';
+      const authUser = { username: 'user', password: 'pass' };
+
+      usersRepository.findUserByUsername?.mockResolvedValueOnce(user);
+      usersRepository.updateSession?.mockImplementationOnce(jest.fn());
+      jwtService.sign?.mockReturnValue(token);
+      jest.spyOn(bcrypt, 'compare').mockImplementationOnce(() => true);
+
+      const result = await authService.signIn(authUser);
+
+      expect(usersRepository.findUserByUsername).toHaveBeenCalledWith(
+        authUser.username,
+      );
+      expect(bcrypt.compare).toHaveBeenCalledWith(
+        authUser.password,
+        user.password,
+      );
+      expect(usersRepository.updateSession).toHaveBeenCalledWith(
+        user.id,
+        token,
+      );
+      expect(result).toMatchObject({ accessToken: token, refreshToken: token });
+    });
+
+    it('should throw UnauthorizedException if user is not valid', async () => {
+      usersRepository.findUserByUsername?.mockResolvedValueOnce(null);
+
+      await expect(
+        authService.signIn({ username: '', password: '' }),
+      ).rejects.toThrowError(UnauthorizedException);
+    });
+
+    it('should throw UnauthorizedException if password is not valid', async () => {
+      const authUser = {
+        password: 'pass',
+        username: '',
+      };
+      const user = {
+        username: '',
+        password: 'other pass',
+      };
+
+      jest.spyOn(bcrypt, 'compare').mockImplementationOnce(() => false);
+      usersRepository.findUserByUsername?.mockResolvedValueOnce(user);
+
+      await expect(authService.signIn(authUser)).rejects.toThrowError(
+        UnauthorizedException,
+      );
+      expect(bcrypt.compare).toHaveBeenCalledWith(
+        authUser.password,
+        user.password,
+      );
+    });
   });
 });
