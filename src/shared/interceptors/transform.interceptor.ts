fc3edc7ac00d27a936dad824faef84da94a6000f
m-zs <mzasada93@gmail.com>
2021-09-24 14:13:32 +0200

    feat: add ignore transform interceptor

--- src/shared/interceptors/transform.interceptor.ts
+++ src/shared/interceptors/transform.interceptor.ts
@@ -7,10 +7,28 @@ import {
 import { classToPlain } from 'class-transformer';
 import { map } from 'rxjs/operators';
 
+const IgnoredPropertyName = Symbol('transform-interceptor');
+
 @Injectable()
 export class TransformInterceptor implements NestInterceptor {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  intercept(_context: ExecutionContext, next: CallHandler<any>) {
+  intercept(context: ExecutionContext, next: CallHandler<any>) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    if ((context.getHandler() as any)[IgnoredPropertyName]) {
+      return next.handle();
+    }
+
     return next.handle().pipe(map((data) => classToPlain(data)));
   }
+}
+
+export function TransformInterceptorIgnore() {
+  return function (
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    _target: any,
+    _propertyKey: string,
+    descriptor: PropertyDescriptor,
+  ) {
+    descriptor.value[IgnoredPropertyName] = true;
+  };
 }
