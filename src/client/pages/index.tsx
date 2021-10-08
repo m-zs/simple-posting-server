95b4e400f0a86261a0ac1975fc65c0ec1b8850b3
m-zs <mzasada93@gmail.com>
2021-10-08 10:27:36 +0200

    refactor: move homepage to pages-implementation folder

--- src/client/pages/index.tsx
+++ src/client/pages/index.tsx
@@ -1,8 +1,1 @@
-import React from 'react';
-import { NextPage } from 'next';
-
-const Home: NextPage = () => {
-  return <h1>test1</h1>;
-};
-
-export default Home;
+export { default } from '../pages-implementation/Home';
