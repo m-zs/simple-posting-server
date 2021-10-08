5694f697eb83956f72c836f1ae79882b684b0de2
m-zs <mzasada93@gmail.com>
2021-10-08 21:59:50 +0200

    build: adjust commands, output

--- src/server/view/services/view.service.ts
+++ src/server/view/services/view.service.ts
@@ -2,13 +2,20 @@ import { Injectable, OnModuleInit } from '@nestjs/common';
 import next from 'next';
 import { NextServer } from 'next/dist/server/next';
 
+import { configService } from 'src/server/config';
+
 @Injectable()
 export class ViewService implements OnModuleInit {
   private server: NextServer;
 
   async onModuleInit(): Promise<void> {
     try {
-      this.server = next({ dev: true, dir: './src/client' });
+      const isProduction = configService.isProduction();
+
+      this.server = next({
+        dev: !isProduction,
+        dir: isProduction ? './src/dist' : './src/client',
+      });
 
       await this.server.prepare();
     } catch (error) {
